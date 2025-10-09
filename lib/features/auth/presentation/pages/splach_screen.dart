95eaa503b78e727b2600cbbf9ae7bbb754e20ef1
Ziyad-000 <Ziyad.sayed1515@compit.aun.edu.eg>
2025-10-09 16:59:57 +0300

    Refactor splash screen and update asset catalog configuration

--- lib/features/auth/presentation/pages/splach_screen.dart
+++ lib/features/auth/presentation/pages/splach_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +11,33 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    loading();
+  }
+
+  void loading() async {
+    await Future.delayed(const Duration(seconds: 2));
+    // Navigator.pushReplacementNamed(context, AppRoutes.loginRoute);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final double height = MediaQuery.heightOf(context);
+    final double width = MediaQuery.widthOf(context);
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            Image.asset('assets/images/app_logo.png', fit: BoxFit.cover),
+            Positioned(
+              top: height / 2 + 90,
+              left: width / 2 - 15,
+              child: CircularProgressIndicator(color: AppColors.accent),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
